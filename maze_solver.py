a99ed8393085d9c609b8deebb85e2ab7246736f4
shyam <bhattaraishyam@gmail.com>
2023-09-02 16:40:12 +0545

    Add code to draw a cell

--- maze_solver.py
+++ maze_solver.py
@@ -43,13 +43,51 @@ class Line:
         canvas.create_line(self.point1.x, self.point1.y, self.point2.x, self.point2.y, fill = fill_color, width = 2)
         canvas.pack()
 
+class Cell:
+    def __init__(self, x1, x2, y1, y2, window):
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        
+        self.__x1 = x1
+        self.__y1 = y1
+        self.__x2 = x2
+        self.__y2 = y2
+
+        self.__win = window 
+
+    def draw(self):
+        top_left_point = Point(self.__x1, self.__y1)
+        bottom_left_point = Point(self.__x1, self.__y2)
+        top_right_point = Point(self.__x2, self.__y1)
+        bottom_right_point = Point(self.__x2, self.__y2)
+
+        if self.has_left_wall:
+            left_wall = Line(top_left_point, bottom_left_point)
+            self.__win.draw_line(left_wall, "red")
+
+        if self.has_right_wall:
+            right_wall = Line(top_right_point, bottom_right_point)
+            self.__win.draw_line(right_wall, "red")
+
+        if self.has_top_wall:
+            top_wall = Line(top_left_point, top_right_point)
+            self.__win.draw_line(top_wall, "red")
+
+        if self.has_bottom_wall:
+            bottom_wall = Line(bottom_left_point, bottom_right_point)
+            self.__win.draw_line(bottom_wall, "red")
+    
+            
+
+
 def main():
     win = Window(800, 600)
 
-    point1 = Point(100,100)
-    point2 = Point(-20,-20)
-    line = Line(point1, point2)
-    win.draw_line(line, "red")
+    cell = Cell(100,200, 300, 400, win)
+    cell.draw()
+
     win.wait_for_close()
 
 main()
